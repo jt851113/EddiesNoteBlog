823d285b0ad8da3592ae02930c22e0c856ee6a4f
jt851113 <33995805+jt851113@users.noreply.github.com>
2022-08-17 22:17:15 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,9 +4,9 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
-  title: "vdoing-template",
+  title: "EddiesNoteBlog",
   description: 'vdoing博客主题模板',
-  base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
+  base: '/EddiesNoteBlog/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
@@ -25,10 +25,10 @@ module.exports = {
         needComments: true,
         // 其他的 Vssue 配置
         autoCreateIssue: true,
-        clientId: "adb9fb0ac1159e00ce7f",
-        clientSecret: "27da8dc85f808c2bd1b6e44da5ae69c4ddf17d8d",
-        owner: "eryajf",
-        repo: "vdoing-template",
+        clientId: "b96a29b29cea43c5a00f",
+        clientSecret: "ebaa1187ba656ba83e969e47981b33a3dca0f448",
+        owner: "jt851113",
+        repo: "EddiesNoteBlog",
       },
     ],
     // 增强 markdown
